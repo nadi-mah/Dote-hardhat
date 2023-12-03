bee1c7b1ab60110c79108b3a87eb4ec5a8a73a75
Nadia Mahyaee <nadiamahyaee@gmail.com>
2023-12-03 14:09:01 +0330

    change

--- contracts/Dote.sol
+++ contracts/Dote.sol
@@ -2,12 +2,9 @@
 
 pragma solidity ^0.8.2 ;
 
-/**@title An electronic Voting application
- * @author Nadia Mahyaee
- */
-
 contract dote {
 
+    
     address public owner; 
 
     struct Voter {
@@ -16,13 +13,14 @@ contract dote {
         bool voted;
     }
     struct Candidate {
-        bytes32 candidateId;
+        string candidateId;
         uint256 numberOfVotes;
     }
 
     mapping (address => Voter) voters;
 
     Candidate[] public candidates;
+    address[] public haveVoted;
     
 
     modifier onlyOwner {
@@ -33,26 +31,26 @@ contract dote {
     constructor() {
     owner = msg.sender;
     }
-
     function addVoter (address _voter) public onlyOwner {
     voters [_voter] = Voter (_voter, 0, false);
     }
 
-    function addCandidate(bytes32 _id) public   {
+    function addCandidate(string memory _id) public   {
         candidates.push(Candidate(_id, 0));
     }
 
 
     function vote (uint256 _candidate)  public {
-        require(voters[msg.sender].voted == false, "You already have voted");
+        require(voters[msg.sender].voted == false, "You have already voted");
         require(_candidate < candidates.length, "Candidate index is out of range");
         voters[msg.sender].vote = _candidate;
         voters[msg.sender].voted = true;
         candidates[_candidate].numberOfVotes += 1;
+        haveVoted.push(msg.sender);
     }
 
-    function winner ()view public returns (bytes32){
-        bytes32  theWinner;
+    function winner ()view public returns (string memory){
+        string memory theWinner;
         uint256 maxVote = 0;
         for (uint256 i=0; i < candidates.length; i++){
             if (candidates[i].numberOfVotes > maxVote){
@@ -62,9 +60,18 @@ contract dote {
         }
         return theWinner;
         }
-    function showCandidates () view public returns(uint256){
-        return candidates.length;
+
+    function showCandidates ()  public view returns(string[] memory){
+        string[] memory candidatesName  = new string[](candidates.length);
+        for (uint256 i = 0; i < candidates.length; i++){
+            candidatesName[i] = candidates[i].candidateId;
+        }
+        return candidatesName;
     }
-    uint256 public numCandidates = showCandidates();
+
+    function showVoters () view public  returns (address[] memory) {
+        return haveVoted;
+    }
+
 
 }
